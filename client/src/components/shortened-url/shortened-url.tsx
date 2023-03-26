1ae66df2e039dd3a6735e87def23beed822c3ae1
rohitman47 <rohitreddy.gangwar@gmail.com>
2023-03-26 18:27:09 +0530

    Add link icon

--- client/src/components/shortened-url/shortened-url.tsx
+++ client/src/components/shortened-url/shortened-url.tsx
@@ -1,38 +1,42 @@
-import styles from "./shortened-url.module.scss";
-import { useState } from "react";
-import CopyIcon from "@assets/icons/copy.svg";
-import QRCodeIcon from "@assets/icons/qr-code.svg";
-import { AlertDialog, QrCode } from "@components";
-import { useAlert } from "@hooks";
+import styles from './shortened-url.module.scss';
+import { useState } from 'react';
+import CopyIcon from '@assets/icons/copy.svg';
+import QRCodeIcon from '@assets/icons/qr-code.svg';
+import ExternalLinkIcon from '@assets/icons/external-link.svg';
+import { AlertDialog, QrCode } from '@components';
+import { useAlert } from '@hooks';
 
 export function ShortenedUrl({ shortURL }: { shortURL: string }) {
-  const [showQrCode, setShowQrCode] = useState(false);
-  const { alertMessage, setAlertMessage } = useAlert(3000);
+	const [showQrCode, setShowQrCode] = useState(false);
+	const { alertMessage, setAlertMessage } = useAlert(3000);
 
-  const copyUrlHandler = async () => {
-    await navigator.clipboard.writeText(shortURL);
-    setAlertMessage("URL copied!");
-  };
+	const copyUrlHandler = async () => {
+		await navigator.clipboard.writeText(shortURL);
+		setAlertMessage('URL copied!');
+	};
 
-  const toggleQrCodeHandler = () => {
-    setShowQrCode((state) => !state);
-  };
+	const toggleQrCodeHandler = () => {
+		setShowQrCode((state) => !state);
+	};
 
-  return (
-    <div className={styles.container}>
-      <p aria-label="Shortened URL" className={styles.shortUrl}>
-        {shortURL}
-      </p>
-      <div className={styles.btns}>
-        <button className={styles.copyBtn} onClick={copyUrlHandler} title="Copy shortened URL">
-          <CopyIcon />
-        </button>
-        <button className={styles.qrCodeBtn} onClick={toggleQrCodeHandler} title="Show QR code">
-          <QRCodeIcon />
-        </button>
-      </div>
-      <QrCode shortURL={shortURL} showQrCode={showQrCode} setShowQrCode={setShowQrCode} />
-      {alertMessage && <AlertDialog message={alertMessage} type="info" />}
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<p aria-label="Shortened URL" className={styles.shortUrl}>
+				<span>{shortURL}</span>
+				<a href={shortURL} target="_blank">
+					<ExternalLinkIcon />
+				</a>
+			</p>
+			<div className={styles.btns}>
+				<button className={styles.copyBtn} onClick={copyUrlHandler} title="Copy shortened URL">
+					<CopyIcon />
+				</button>
+				<button className={styles.qrCodeBtn} onClick={toggleQrCodeHandler} title="Show QR code">
+					<QRCodeIcon />
+				</button>
+			</div>
+			<QrCode shortURL={shortURL} showQrCode={showQrCode} setShowQrCode={setShowQrCode} />
+			{alertMessage && <AlertDialog message={alertMessage} type="info" />}
+		</div>
+	);
 }
